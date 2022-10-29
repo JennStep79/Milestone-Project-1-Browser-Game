98a0f116da620b64a4c3332155c1006d680a6b2a
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-10-29 10:27:54 -0700

    first commit, set up repo and file folder

---
+++ script.js
@@ -0,0 +1,35 @@
+// create event listeners for clicked cards
+
+// add function to show flipped card value (faceup)
+
+// function to check for matches
+
+// function to reset unmatched cards back to facedown
+
+// function to clear matched cards from game board
+
+// event listeners to play sound when turned over (animal sound or intrument sound or whoosh)
+
+// event listeners to play ding ding sound when a match is found
+
+// message to player that match was found
+
+// play error sound when no match
+
+// play victory song when game board is cleared
+
+// message to user that game was won
+
+// timer clock function
+
+// start timer when game is started (on first click?)
+
+// warning sound when time is running out
+
+// buzzer when time is up
+
+// message to user that game was lost
+
+// play again? message 
+
+// function to reset (shuffle) cards and reset timer
